2a2679b285b61a66d0c83ef6c5e6a166d986f2c2
unknown <fahdelattar13@gmail.com>
2025-08-23 05:43:02 +0100

    added a bit of responsiveness

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,8 +7,8 @@ export const Layout = ({children}) => {
     return (
         <>
             <LeftSidebar />
-            <div className="grid grid-cols-[13vw_1fr_13vw] min-h-screen">
-                <div className="col-start-2">
+            <div className="grid sm:grid-cols-[13vw_1fr_13vw] px-6 sm:px-0 min-h-screen">
+                <div className="sm:col-start-2 overflow-hidden">
                     <Header />
                     <div className="container grid grid-cols-1 gap-28">
                         {children}
