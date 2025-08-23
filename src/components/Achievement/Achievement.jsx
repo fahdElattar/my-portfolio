0bbb4b21fd0ae04b8d11e6e628a28ccd0fed8df4
unknown <fahdelattar13@gmail.com>
2025-08-23 05:52:15 +0100

    added a bit of responsiveness

--- src/components/Achievement/Achievement.jsx
+++ src/components/Achievement/Achievement.jsx
@@ -4,7 +4,7 @@ export const Achievement = ({score, title}) => {
     return (
         <div className="achievement">
             <h1 className="achievement-score">{score}+</h1>
-            <span className="achievement-title text-text-md text-nowrap">{title}</span>
+            <span className="achievement-title text-text-md text-nowrap text-center">{title}</span>
         </div>
     )
 }
