6cd81e27872a59b194be4114903ab908d77ccd3e
unknown <fahdelattar13@gmail.com>
2025-08-27 01:56:43 +0100

    added servicesSection

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -2,12 +2,12 @@ import { Layout } from "@/components/Layout/Layout";
 import { Projects } from "@/components/Sections/Projects/Projects";
 import { Stacks } from "@/components/Sections/Stacks/Stacks";
 import { Words } from "@/components/Sections/Words/Words";
-import { Services } from "@/components/Sections/Services/Services";
+import { Services as ServicesSection } from "@/components/Sections/Services/Services";
 
 export default function Services() {
     return (
         <Layout activeSidebarLink="services">
-            <Services />
+            <ServicesSection />
             <Stacks />
             <Projects />
             <Words />
