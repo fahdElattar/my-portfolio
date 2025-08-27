d941a4914fc5585d37e5587a10991f1590ec45f7
unknown <fahdelattar13@gmail.com>
2025-08-27 01:46:32 +0100

    added services section to services page

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,22 +1,13 @@
 import { Layout } from "@/components/Layout/Layout";
-import { SectionHead } from "@/components/SectionHead/SectionHead";
 import { Projects } from "@/components/Sections/Projects/Projects";
 import { Stacks } from "@/components/Sections/Stacks/Stacks";
 import { Words } from "@/components/Sections/Words/Words";
+import { Services } from "@/components/Sections/Services/Services";
 
 export default function Services() {
     return (
         <Layout activeSidebarLink="services">
-            <section id="Services">
-                <SectionHead
-                    title={"My Services"}
-                    iconName={"thunder"}
-                    description={"Formulating comprehensive strategies to meet your design goals and exceed expectations."}
-                />
-                <div className="services-list grid grid-cols-1 md:grid-cols-2 gap-3">
-                    {/* <ServiceItem /> */}
-                </div>
-            </section>
+            <Services />
             <Stacks />
             <Projects />
             <Words />
