360dd18b9ef49f37f5ae614013876d2ee6f6a67d
unknown <fahdelattar13@gmail.com>
2025-09-05 11:34:22 +0100

    few adjustments

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,10 @@
-import { Hero } from "@/components/Sections/Hero/Hero";
 import { Experience } from "@/components/Sections/Experience/Experience";
 import { Stacks } from "@/components/Sections/Stacks/Stacks";
 import { Services } from "@/components/Sections/Services/Services";
 import { Words } from "@/components/Sections/Words/Words";
 import { Projects } from "@/components/Sections/Projects/Projects";
 import { Layout } from "@/components/Layout/Layout";
+import { Hero } from "@/components/Sections/Hero/Hero";
 
 const Home = () => {
 	return (
